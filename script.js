470e7ec61843d765f5afe11e7b161649621d9932
odisey228 <24a_ums@liceum.ztu.edu.ua>
2025-02-18 12:22:48 +0200

    зробив сайт про день валентина додав кнопку

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('btn_love_wishes').addEventListener('click',function(){
+    document.getElementById('love-wishes').innerText = "Нехай кожен день приносить радість і тепло!"})
