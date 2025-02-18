1475d5f625fddea05b278460d199fbd4aaaee695
odisey228 <24a_ums@liceum.ztu.edu.ua>
2025-02-18 12:57:53 +0200

    додав кнопку додав стиль кнопки додав пару фото додав побажання

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-document.getElementById('btn_love_wishes').addEventListener('click',function(){
-    document.getElementById('love-wishes').innerText = "Нехай кожен день приносить радість і тепло!"})
+const arrayofLoveWishes=[
+"Нехай це свято подарує радість і тепло.",
+"Бажаю тобі щирого кохання і ніжності.",
+"Кохання окриляє, надихає і дарує щастя.",
+"Хай у серці завжди живе справжня любов.",
+"Любов – це магія, що наповнює життя сенсом.",
+"Нехай поруч буде людина, яка цінує тебе.",
+"Хай почуття будуть щирими, світлими і вічними.",
+"Нехай серце палає від ніжності та тепла.",
+"Кохання – це світло, що веде нас уперед.",
+"Бажаю тобі щастя, романтики і приємних сюрпризів!"
+
+]
+
+
+
+
+
+
+document.getElementById('btn_love_wishes').addEventListener('click',()=>{
+    let index=Math.floor(Math.random() * arrayofLoveWishes.length)
+
+    document.getElementById('love-wishes').innerText = arrayofLoveWishes[index]})
